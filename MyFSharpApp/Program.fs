fd4c769332338e61d5778790578dd29ae044dc76
bipanajirel <Bipana.Jirel@triosstudent.com>
2024-03-22 15:19:38 -0400

    V1

---
+++ MyFSharpApp/Program.fs
@@ -0,0 +1,16 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+//Bipana Jirel
+//22071147
+printfn " Bipana Jirel"
+[1..100] |> List.sum |> printfn "sum=%d"
+// automatic equality and comparison
+type Person = { First:string; Last:string }
+let person1 = { First = "John"; Last = "Doe" }
+let person2 = { First = "Alex"; Last = "Lan" }
+printfn "Equal? %A" (person1 = person2)
+// strict type checking
+printfn "print string %s" 123
+//compile error // all values immutable by default
+person1.First <- "new name" //assignment error
+// impure code(ability to assign) when needed
+let mutable counter = 0
---
+++ TestCsharp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TestCsharp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
